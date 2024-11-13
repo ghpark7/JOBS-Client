b41b579338521d2e09f206635f1863cd0e307c2f
Geonhwi Park <ghpark715@gmail.com>
2024-11-13 21:27:04 +0900

    Feat: Home

--- jobs/src/App.js
+++ jobs/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/mainPage/Main';
 import Input from './components/inputPage/Input';
+import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Home />} />
         <Route path="/input" element={<Input />} />
       </Routes>
     </Router>
